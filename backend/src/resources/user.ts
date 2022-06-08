bf5bc497a4db57150411634f7309a8d2e9a93731
Matej Kubik <matelf.kubik@gmail.com>
2022-06-09 01:50:08 +0200

    Moved userGoals logic to separate file

--- backend/src/resources/user.ts
+++ backend/src/resources/user.ts
@@ -1,6 +1,7 @@
 import { object, string, number, ValidationError, date } from "yup";
 import { Request, Response } from "express";
 import prisma from "../client";
+import { userDetails, userGoals } from "../resources";
 
 const userCredentialsSchema = object({
   passwordHash: string().required(),
@@ -49,12 +50,11 @@ export const register = async (req: Request, res: Response) => {
     req.body.details.userId = user.id;
     const details = await userDetailsSchema.validate(req.body.details);
 
-    const result = await prisma.userDetails.create({
-      data: {
-        ...details,
-      },
-    });
-    // TODO check whether userDetails created successfully
+    const age = getAge(new Date(details.birthdate));
+    let goals =
+      req.body.goals ||
+      calculateGoals(age, details.height, details.sex, details.weight);
+    await userGoals.store(goals, user.id);
 
     const session = await prisma.sessions.create({
       data: {
@@ -184,3 +184,26 @@ const validateAuthorization = async (sessionId: string, userId: string) => {
   });
   return session?.userId === userId;
 };
+
+const calculateGoals = (
+  age: number,
+  height: number,
+  sex: number,
+  weight: number
+) => {
+  // FIXME calculate values istead of static data
+  return {
+    calories: 2300,
+    proteins: 150,
+    carbs: 230,
+    fats: 79,
+    fiber: 18,
+    salt: 5,
+  };
+};
+
+// TODO check whether working
+const getAge = (birthDate: Date) =>
+  Math.floor(
+    (new Date().getTime() - new Date(birthDate).getTime()) / 3.15576e10
+  );
