6cb7dd43105df59c74558e716a6b975d3b24114f
Barbora Hornakova <xhornak2@fi.muni.cz>
2022-06-11 17:49:33 +0200

    refactor(FoodDetailsPage): added flex to Hero

--- frontend/src/pages/FoodDetailsPage.tsx
+++ frontend/src/pages/FoodDetailsPage.tsx
@@ -2,20 +2,20 @@ import { styled } from "@mui/system";
 import {CssBaseline} from "@mui/material";
 import {MainAppBar} from "../components/Header";
 import FoodDetails from "../components/FoodDetails"
+import {Hero} from "./MainPage";
 
-interface FoodDetailsPageProps {
-    calories?: number
-}
+const FlexedHero = styled(Hero)({
+  display: "flex",
+  justifyContent: "center",
+});
 
 type FoodDetailsPageType = FC<FoodDetailsPageProps>;
 
 const FoodDetailsPage:FoodDetailsPageType = ({calories}) => {
   return (
-    <Hero>
+    <FlexedHero>
       <CssBaseline />
-      <Header />
-      <Typography sx={{}}>{calories}</Typography>
-    </Hero>
+    </FlexedHero>
   );
 };
 
