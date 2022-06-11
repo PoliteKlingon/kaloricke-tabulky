93c6d5b701b0bb53dd7954a0411471e546bc9f1e
Barbora Hornakova <xhornak2@fi.muni.cz>
2022-06-11 10:40:14 +0200

    feat(fe-App): added FoodDetailsPage to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./page/MainPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import FoodDetailsPage from "./pages/FoodDetailsPage";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { useEffect, useContext } from "react";
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/details" element={<FoodDetailsPage />} />
         </Routes>
       </BrowserRouter>
     </LocalizationProvider>
