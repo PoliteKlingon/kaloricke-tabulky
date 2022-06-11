66953fee24d009a7f86146105640e049178c4a81
Barbora Hornakova <xhornak2@fi.muni.cz>
2022-06-11 21:56:26 +0200

    fix(FoodDetailsPage): changed source for banan img

--- frontend/src/pages/FoodDetailsPage.tsx
+++ frontend/src/pages/FoodDetailsPage.tsx
@@ -11,7 +11,7 @@ const FlexedHero = styled(Hero)({
 
 const FoodDetailsPage = () => {
   // TODO: development only remove later
-  const tempData = { "name": "banan", "photo": "www.pornhub.com/asduixciyxuzcvb", "description": "asdkfalsjdfk;ljds", "caloric_value": 123, "sugar": 69, "salt": 4, "carbs": 22, "proteins": 1, "fats": 2, "saturated_fats": 1, "fiber": 69, "id": "314reajfo34u" }
+  const tempData = { "name": "banan", "photo": "https://www.lovemysalad.com/sites/default/files/styles/home_carousel_item_768/public/banaan-large.jpg?itok=qRLZa1EH", "description": "asdkfalsjdfk;ljds", "caloric_value": 123, "sugar": 69, "salt": 4, "carbs": 22, "proteins": 1, "fats": 2, "saturated_fats": 1, "fiber": 69, "id": "314reajfo34u" };
 
   return (
     <FlexedHero>
