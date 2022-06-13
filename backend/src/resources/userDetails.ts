a4922dd5a039fba97f098e2ca8afd85850594314
Matej Kubik <matelf.kubik@gmail.com>
2022-06-13 13:46:29 +0200

    Fixed problem with date in string format.

--- backend/src/resources/userDetails.ts
+++ backend/src/resources/userDetails.ts
@@ -1,10 +1,9 @@
 import prisma from "../client";
 import { Request, Response } from "express";
 import { date, number, object, string, ValidationError } from "yup";
-import { getUserIdFromSessionId, validateAuthorization } from "./user";
+import { validateAuthorization } from "./user";
 import { sendInternalServerError, sendSuccess } from "./universalResponses";
 
-// TODO fix validation not accepting "yyyy-dd-mm" format
 const detailsSchema = object({
   username: string().required().trim(),
   name: string().required().trim(),
@@ -16,7 +15,6 @@ const detailsSchema = object({
   email: string().email().required().trim(),
   userId: string().required(),
 }).noUnknown(true);
-// .strict();
 
 const detailsUpdateSchema = object({
   username: string().optional().trim(),
@@ -28,9 +26,8 @@ const detailsUpdateSchema = object({
   sex: number().optional().min(0).max(1),
   email: string().email().optional().trim(),
 }).noUnknown(true);
-// .strict();
 
-/* This is used only by `user` module in method `register` */
+/* This should not be used directly as endpoint method */
 export const store = async (data: any, userId: string) => {
   data.userId = userId;
   const details = await detailsSchema.validate(data);
@@ -40,44 +37,15 @@ export const store = async (data: any, userId: string) => {
   });
 };
 
+/* This should not be used directly as endpoint method */
 export const update = async (userId: string, reqData: any) => {
   const data = await detailsUpdateSchema.validate(reqData);
+  console.log("Successfully parsed");
   return await prisma.userDetails.update({
     where: { userId: userId },
     data: { ...data },
   });
 };
-
-// export const update = async (req: Request, res: Response) => {
-//   const userId = await getUserIdFromSessionId(req.body.sessionId);
-//   if (!userId) return sendNotFound(res, "User not found");
-
-//   try {
-//     if (req.body.details?.email) {
-//       const duplicate = await prisma.userDetails.findUnique({
-//         where: { email: req.body.details.email },
-//       });
-//       if (duplicate && duplicate.userId != userId) {
-//         return sendDuplicateError(res, "User with given email already exists");
-//       }
-//     }
-
-//     const result = updateOnly(userId, req.body.details);
-
-//     if (req.body.details?.email) {
-//       prisma.userCredentials.update({
-//         where: { userId: userId },
-//         data: { email: req.body.details.email },
-//       });
-//     }
-
-//     return sendSuccess(res, "User data updated successfully", result);
-//   } catch (e) {
-//     if (e instanceof ValidationError) return sendValidationError(res, e);
-
-//     return sendInternalServerError(res);
-//   }
-// };
 
 export const get = async (req: Request, res: Response) => {
   if (!(await validateAuthorization(req.body.sessionId, req.body.userId)))
