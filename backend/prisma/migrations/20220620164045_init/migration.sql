24377794f83baad56a160dc3b116d791c1197f36
Matej Kubik <matelf.kubik@gmail.com>
2022-06-20 19:58:29 +0200

    Added relation between user and session

--- backend/prisma/migrations/20220620164045_init/migration.sql
+++ backend/prisma/migrations/20220620164045_init/migration.sql
@@ -72,7 +72,8 @@ CREATE TABLE "Meal" (
 -- CreateTable
 CREATE TABLE "Sessions" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
