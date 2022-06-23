9b4dc8322039e7a93ea8abfa6eb9a37872621f1c
Barbora Hornakova <xhornak2@fi.muni.cz>
2022-06-23 17:27:42 +0200

    feat(FoodDetailsPage): added getting data from backend

--- frontend/src/pages/FoodDetailsPage.tsx
+++ frontend/src/pages/FoodDetailsPage.tsx
@@ -1,8 +1,11 @@
 import { styled } from "@mui/system";
 import {CssBaseline} from "@mui/material";
 // import {MainAppBar} from "../components/Header";
-import FoodDetails from "../components/FoodDetails";
+import FoodDetails, {Food} from "../components/FoodDetails";
 import {Hero} from "./MainPage";
+import axios from "../api/axios";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 
 const FlexedHero = styled(Hero)({
   display: "flex",
@@ -11,13 +14,24 @@ const FlexedHero = styled(Hero)({
 
 const FoodDetailsPage = () => {
   // TODO: development only remove later
-  const tempData = { "name": "banan", "photo": "https://www.lovemysalad.com/sites/default/files/styles/home_carousel_item_768/public/banaan-large.jpg?itok=qRLZa1EH", "description": "asdkfalsjdfk;ljds", "caloric_value": 123, "sugar": 69, "salt": 4, "carbs": 22, "proteins": 1, "fats": 2, "saturated_fats": 1, "fiber": 69, "id": "314reajfo34u" };
+  const tempData = { "name": "banan", "photo": "https://www.lovemysalad.com/sites/default/files/styles/home_carousel_item_768/public/banaan-large.jpg?itok=qRLZa1EH", "description": "asdkfalsjdfk;ljds", "calories": 123, "sugar": 69, "salt": 4, "carbs": 22, "proteins": 1, "fats": 2, "saturated_fats": 1, "fiber": 69, "id": "314reajfo34u" };
+
+  const [food, setFood] = useState<Food>(tempData);
+  let { id } = useParams();
+  useEffect(() => {
+    axios
+      .get(`/food/${id}`)
+      .then((response) => {
+        console.log(response.data)
+        setFood(response.data.data)
+      });
+  })
 
   return (
     <FlexedHero>
       <CssBaseline />
       {/*<MainAppBar />*/}
-      <FoodDetails food={tempData}/>
+      <FoodDetails food={food}/>
     </FlexedHero>
   );
 };
