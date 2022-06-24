5d8dfa1c05bc4186073bd17d638af1d22c4b677c
Jonáš Jadrníček <jon.jadrnicek@gmail.com>
2022-06-25 00:05:32 +0200

    fixed threshold to remove overlap

--- frontend/src/components/Utils/HideOnScroll.tsx
+++ frontend/src/components/Utils/HideOnScroll.tsx
@@ -2,7 +2,7 @@ import { Slide } from "@mui/material";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 
 const HideOnScroll = ({ children }: any) => {
-  const trigger = useScrollTrigger({ disableHysteresis: true });
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 0 });
   return (
     <Slide appear={false} direction="down" in={!trigger}>
       {children}
