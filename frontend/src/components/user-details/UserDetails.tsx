c6815f5c93cd6a1d13bd68f17827320ed8e001c5
Jiří Loun <514665@mail.muni.cz>
2022-06-25 15:57:36 +0200

    feat: deleteAccountModal created succesfully

--- frontend/src/components/user-details/UserDetails.tsx
+++ frontend/src/components/user-details/UserDetails.tsx
@@ -45,6 +45,18 @@ import {
       transform: "scale(1.3)",
     },
   });
+
+  const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   
   const Root = styled("div")({
     display: "flex",
@@ -1470,15 +1482,54 @@ import {
                 </Button>
                 <Modal
                   open={deleteAccountModal}
-                  //onClose={handleClose}
+                  onClose={() => setDeleteAccountModal(false)}
                 >
-                  <Box sx={{background: "white"}}>
+                  <Box sx={modalStyle}>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
-                      Text in a modal
+                      Opravdu chcete zrušit svůj účet?
                     </Typography>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                      Všechna Vaše data budou tímto krokem odstraněna.
                     </Typography>
+                    <Button
+                      variant="contained"
+                      disableRipple
+                      sx={{
+                        backgroundColor: "orange",
+                        fontWeight: "bold",
+                        fontFamily: "Nunito",
+                        marginY: 1,
+                        marginRight: 2,
+                        width: 100,
+                        transition: "transform 0.2s",
+                        ":hover": {
+                          transform: "scale(1.1)",
+                          backgroundColor: "#f29830",
+                        },
+                      }}
+                      onClick={() => {
+                        setDeleteAccountModal(false);
+                      }}
+                    >Zpět</Button>
+                    <Button
+                      variant="contained"
+                      disableRipple
+                      sx={{
+                        backgroundColor: "red",
+                        fontWeight: "bold",
+                        fontFamily: "Nunito",
+                        marginY: 1,
+                        width: 200,
+                        transition: "transform 0.2s",
+                        ":hover": {
+                          transform: "scale(1.1)",
+                          backgroundColor: "darkRed",
+                        },
+                      }}
+                      onClick={() => {
+                        //TODO: delete účtu
+                      }}
+                    >Odstranit účet</Button>
                   </Box>
                 </Modal>
               </Grid>
