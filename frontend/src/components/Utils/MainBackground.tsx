003741e477063123fe1cd3599b4bff48adea2cd5
Jiří Loun <514665@mail.muni.cz>
2022-06-26 22:34:28 +0200

    fix: removed height argument

--- frontend/src/components/Utils/MainBackground.tsx
+++ frontend/src/components/Utils/MainBackground.tsx
@@ -8,7 +8,6 @@ const MainBackground = styled("div")({
   backgroundSize: "cover",
   backgroundPosition: "center",
   backgroundAttachment: "fixed",
-  height: "100vh"
 });
 
 export default MainBackground;
