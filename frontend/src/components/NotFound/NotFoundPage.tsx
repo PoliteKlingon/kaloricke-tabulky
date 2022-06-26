8f0cb4592bb1aa6739b6b3712092ffd1d4477139
Jiří Loun <514665@mail.muni.cz>
2022-06-26 21:47:11 +0200

    fix: fix conflict

--- frontend/src/components/NotFound/NotFoundPage.tsx
+++ frontend/src/components/NotFound/NotFoundPage.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import { Box, CssBaseline } from "@mui/material";
 import NotFound from "./NotFound";
 import CustomAppBar from "../Utils/CustomAppBar";
@@ -22,31 +20,3 @@ const NotFoundPage = () => {
 };
 
 export default NotFoundPage;
-=======
-import { styled } from "@mui/system";
-import { CssBaseline } from "@mui/material";
-import AddFood from "./AddFood";
-
-
-
-const Hero = styled("div")({
-  backgroundImage: `url(${
-    import.meta.env.VITE_PUBLIC_URL + "/assets/background.jpg"
-  })`,
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  minHeight: "100vh"
-});
-
-const AddFoodPage = () => {
-  return (
-    <Hero>
-      <CssBaseline />
-      <AddFood />
-    </Hero>
-  );
-};
-
-export default AddFoodPage;
->>>>>>> 70ab42ff68abf67ae3e5df5e77b179b707f0b7cc
--- frontend/src/components/UserDetails/UserDetailsPage.tsx
+++ frontend/src/components/UserDetails/UserDetailsPage.tsx
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import { Box, CssBaseline } from "@mui/material";
 import UserDetails from "./UserDetails";
 import MainBackground from "../Utils/MainBackground";
 import CustomAppBar from "../Utils/CustomAppBar";
 
-const MainPage = () => {
+const UserDetailsPage = () => {
   return (
     <MainBackground>
       <Box
@@ -17,33 +16,6 @@ const MainPage = () => {
         <UserDetails />
       </Box>
     </MainBackground>
-=======
-import { styled } from "@mui/system";
-import { CssBaseline } from "@mui/material";
-import Header from "../Header";
-import Features from "../Features";
-import UserDetails from "./UserDetails";
+  )}
 
-
-
-const Hero = styled("div")({
-  backgroundImage: `url(${
-    import.meta.env.VITE_PUBLIC_URL + "/assets/background.jpg"
-  })`,
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  minHeight: "100vh"
-});
-
-const MainPage = () => {
-  return (
-    <Hero>
-      <CssBaseline />
-      <UserDetails />
-    </Hero>
->>>>>>> 70ab42ff68abf67ae3e5df5e77b179b707f0b7cc
-  );
-};
-
-export default MainPage;
+  export default UserDetailsPage;
