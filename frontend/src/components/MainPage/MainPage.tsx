0bcd88164d1c28d183ed96f13dcef5c60c9955ec
Jiří Loun <514665@mail.muni.cz>
2022-06-27 23:32:36 +0200

    feat: redirect  from '/'

--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 
-import { CssBaseline } from "@mui/material";
+import { CssBaseline } from "@mui/material"
+import AuthContext from "../../context/AuthProvider";
+import { Navigate } from "react-router-dom"
 
 import Header from "./Header";
 import Features from "./Features";
@@ -10,8 +12,11 @@ import CustomAppBar from "../Utils/CustomAppBar";
 interface IMainPageProps {}
 
 const MainPage:FC<IMainPageProps> = () => {
+  // @ts-ignore
+  const { auth } = useContext(AuthContext);
   return (
     <MainBackground>
+      {auth != null && auth.username != null && auth?.username != "" && <Navigate to="/home" />}
       <CssBaseline />
       <CustomAppBar/>
       <Header />
